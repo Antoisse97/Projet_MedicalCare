4e511d874454f439838b9de91047af2e77d34076
Calebams237 <amoussoubonheur@gmail.com>
2026-03-18 09:51:27 +0100

    Create AutomatisationMedicalCare.sql
    Ajout d'un fichier pour les automatisations.

---
+++ AutomatisationMedicalCare.sql
@@ -0,0 +1,59 @@
+--trigger uptade de patient après la saisit de son dossier
+CREATE OR REPLACE TRIGGER trg_uptadePatient_AprèsSaisitDossier
+AFTER INSERT 
+ON DOSSIER
+FOR EACH ROW 
+BEGIN
+    UPDATE PATIENT 
+    SET LIGNE_DOSSIER = :NEW.LIGNE_DOSSIER
+    WHERE ID_PATIENT = :NEW.ID_PATIENT;
+END;
+/
+COMMIT
+
+--numéros adéli 
+CREATE OR REPLACE TRIGGER trg_adeli_medecin
+BEFORE INSERT ON PERSO_MED
+FOR EACH ROW
+DECLARE
+    v_role PERSONNEL.role%type;
+    v_base NUMBER;
+BEGIN 
+-- récupération du role du personnage 
+    SELECT ROLE
+    INTO v_role
+    FROM PERSONNEL
+    WHERE ID_PERSO = :NEW.ID_PERSO;
+-- base selon le role 
+    IF v_role = 'Medecin' THEN
+        v_base :=1000;
+    ELSIF v_role = 'Infirmiere' THEN 
+        v_base :=2000;
+    ELSIF v_role = 'Cardiologue' THEN 
+        v_base :=3000;
+    ELSIF v_role = 'KINE' THEN 
+        v_base :=4000;
+    ELSIF v_role = 'Biologiste' THEN 
+        v_base :=5000;
+    ELSE 
+        v_base := 9000; --valeur default
+        RAISE_APPLICATION_ERROR(-20010, 'Le role saisit est inconu');
+    END IF; 
+-- calcul du num adeli 
+    :NEW.NUM_ADELI := v_base + :NEW.ID_PERSO;
+END;
+/
+COMMIT;
+
+--trigger uptade numéro adéli  -> Perso_Med après la saisit d'un Personnel
+CREATE OR REPLACE TRIGGER trg_uptadePerso_Med_aprèsSaisitPersonnel
+AFTER INSERT ON PERSONNEL 
+FOR EACH ROW 
+BEGIN 
+    UPDATE PERSO_MED 
+    SET Num_Adeli = :NEW.Num_Adeli
+    WHERE Id_Perso = :NEW.Id_Perso;
+END; 
+/
+COMMIT
+-- prêt pour être testé 
