ce5dc660565e1d38d0a5f71b7f5bf9a147663f0d
Antoisse97 <assoumany.ant@gmail.com>
2026-03-25 12:31:08 +0100

    automatisation calcul lot

--- AutomatisationMedicalCare.sql
+++ AutomatisationMedicalCare.sql
@@ -58,3 +58,23 @@ END;
 COMMIT
 -- prêt pour être testé 
 ---- 24 Mars 2026
+---- 25 Mars 2026
+ALTER TABLE FICHE_QUOTIDIENNE RENAME COLUMN NUMPATIENT TO NUM_JOUR;
+--Générer automatiquement NUMLOTS (par concaténation de l’ID_PATIENT et du numéro de jour) plutôt que de le saisir à la main.
+CREATE OR REPLACE TRIGGER trg_CalculNumLot  --pret a tester
+BEFORE INSERT ON LOT_MEDICAMENT 
+FOR EACH ROW
+DECLARE 
+    v_numJour number;
+BEGIN
+    --recupération du num de fiche
+    SELECT NUM_JOUR
+    INTO v_numJour
+    FROM FICHE_QUOTIDIENNE
+    WHERE ID_PATIENT = :NEW.ID_PATIENT;
+    
+    -- Calcul du numéros de lot
+    :NEW.NUMLOTS := v_numJour * 1000 + :NEW.ID_PATIENT;
+END;
+/
+commit
