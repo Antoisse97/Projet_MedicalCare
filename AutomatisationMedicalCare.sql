ea28bf31a5349bc3f6c2a6ec5c26ff64daf1c50a
Calebams237 <amoussoubonheur@gmail.com>
2026-04-03 12:24:46 +0200

    Update AutomatisationMedicalCare.sql
    Ajout de la Procédure de peuplement et de nettoyage du personnel

--- AutomatisationMedicalCare.sql
+++ AutomatisationMedicalCare.sql
@@ -96,18 +96,27 @@ call PeupleCentre(5);
 SELECT * FROM CENTRE;
 commit
 
---01 Avril 2026
+--01 Avril 2026 (Développé par C)
 -- Automatisation de la numérotation des tables dont l'identifiant peut être numéroté automatiquement à partir de 1 : 
--- creation d'une sequence de numérotation automatique
-create sequence NumerotationAutoSeq 
+-- creation d'une sequence de numérotation automatique pour le patient
+create sequence NumerotationPatientSeq 
     start with 1 increment by 1; -- Commence la numérotation à 1 mais pour tester on peut modifier le chiffre de départ
     
+-- creation d'une sequence de numérotation automatique pour le centre
+create sequence NumerotationCentreSeq
+    start with 1 increment by 1;
+
+-- creation d'une sequence de numérotation automatique pour le personnel
+create sequence NumerotationPersonnelSeq
+    start with 11 increment by 1;
+
+
 -- Trigger d'automatisation de la numérotation du patient 
 create or replace trigger trg_PatientAutoNum
 before insert on Patient
 for each row
 begin 
-    select NumerotationAutoSeq.nextval into :NEW.Id_Patient from dual; -- Insert la valeur à partir de la séquence de numérotation
+    select NumerotationPatientSeq.nextval into :NEW.Id_Patient from dual; -- Insert la valeur à partir de la séquence de numérotation
 end; 
 -- Pour tester il faut mettre un null à l'emplacement de Id_Patient
 
@@ -122,7 +131,7 @@ create or replace trigger trg_CentreAutoNum
 before insert on Centre
 for each row
 begin 
-    select NumerotationAutoSeq.nextval into :NEW.Id_Centre from dual; -- Insert la valeur à partir de la séquence de numérotation
+    select NumerotationCentreSeq.nextval into :NEW.Id_Centre from dual; -- Insert la valeur à partir de la séquence de numérotation
 end; 
 -- Pour tester il faut mettre un null à l'emplacement de Id_Centre
 
@@ -133,9 +142,90 @@ create or replace trigger trg_PersonnelAutoNum
 before insert on Personnel
 for each row
 begin 
-    select NumerotationAutoSeq.nextval into :NEW.Id_Perso from dual; -- Insert la valeur à partir de la séquence de numérotation
+    select NumerotationPersonnelSeq.nextval into :NEW.Id_Perso from dual; -- Insert la valeur à partir de la séquence de numérotation
 end; 
 -- Pour tester il faut mettre un null à l'emplacement de Id_Perso
 /
 commit;
+
+-- 03 Avril 2026
+-- Procédure de peuplement du personnel (développé par C)
+CREATE OR REPLACE PROCEDURE PeuplePersonnel (np IN NUMBER) AS
+    TYPE t_roles IS VARRAY(6) OF VARCHAR2(30); -- tableau de valeurs pour le role
+    v_roles t_roles := t_roles('Medecin', 'Infirmier', 'ARC', 'KINE', 'Cardiologue', 'Biologiste');
+
+    TYPE t_centres IS VARRAY(4) OF NUMBER; -- tableau de valeurs pour le centre
+    v_centres t_centres := t_centres(1, 2, 3, 4);
+
+    TYPE t_identites IS VARRAY(20) OF VARCHAR2(50); -- tableau de valeurs pour les identites
+    v_identites t_identites := t_identites(
+      'Martin Nathan',
+      'Dubois Emma',
+      'Bernard Lucas',
+      'Thomas Chloé',
+      'Robert Hugo',
+      'Richard Inès',
+      'Petit Adam',
+      'Durand Léa',
+      'Leroy Noah',
+      'Moreau Manon',
+      'Simon Louis',
+      'Laurent Sarah',
+      'Lefebvre Jules',
+      'Michel Clara',
+      'Garcia Tom',
+      'David Camille',
+      'Bertrand Lina',
+      'Roux Maxime',
+      'Vincent Zoé',
+      'Fournier Aymeric'
+    );
+
+    v_idCentre    NUMBER; 
+    v_sonIdentite VARCHAR2(50);
+    v_metier      VARCHAR2(30);
+    v_index       PLS_INTEGER;
+BEGIN
+    FOR i IN 1..np LOOP -- Boucle 
+        v_index := TRUNC(DBMS_RANDOM.VALUE(1, v_centres.COUNT + 1)); -- on choisit une valeur au hasard
+        v_idCentre := v_centres(v_index);
+
+        v_index := TRUNC(DBMS_RANDOM.VALUE(1, v_identites.COUNT + 1)); -- on choisit une valeur au hasard
+        v_sonIdentite := v_identites(v_index);
+
+        v_index := TRUNC(DBMS_RANDOM.VALUE(1, v_roles.COUNT + 1)); -- on choisit une valeur au hasard
+        v_metier := v_roles(v_index);
+
+        INSERT INTO PERSONNEL (ID_PERSO, ID_CENTRE, NUM_ADELI, NOM, ROLE) -- insertion
+        VALUES (NULL, v_idCentre, NULL, v_sonIdentite, v_metier);
+    END LOOP;
+END;
+/
+commit;
+
+SELECT NumerotationPersonnelSeq.NEXTVAL FROM dual;
+
+--call
+call PeuplePersonnel(15); -- Insère 15 personnel
+
+-- 03 Avril 2026
+-- Procédure de nettoyage du personnel (développé par C)
+-- Procedure permettant d'effacer le contenu à partir d'un nombre de départ 
+CREATE OR REPLACE PROCEDURE CleanPersonnel  (debut IN NUMBER, fin IN NUMBER) AS
+    v_max Number; 
+Begin 
+    select count (*) into v_max from Personnel where ID_PERSO between debut and fin; -- on compte le nombre d'élement à supprimer
+    if fin - debut + 1 > v_max then -- nombre incohérent
+        RAISE_APPLICATION_ERROR(-20040,'Le nombre de personne à supprimer est supérieur au nombre de personnel');
+    end if; 
+    
+    For i in debut..fin loop -- nombre correct
+        Delete from Personnel where ID_PERSO = i; -- suppression
+    end loop; 
+end; 
+/
+commit;
+
+call CleanPersonnel (22,36); -- (Efface les personnel dont les identifiants sont compris entre 22 et 36,les deux inclus)
+
 
